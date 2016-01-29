31147c4a519ff253ca7453a8d5855e381431286c
Erica Lastufka <elastufk@cvpost005.cv.nrao.edu>
2016-01-29 13:35:05 -0500

    updates to staging.py

--- staging.py
+++ staging.py
@@ -22,9 +22,13 @@ import mypre_imaging as mpre
 import project_info
 import IPython
 import multiprocessing #need to make each instance of the loop go at the same time else it'll take forever!!
+import time
 
-def staging_info(project_number, user_name):
-    project_path = '/lustre/naasc/observers/' + user_name + '/'
+def staging_info(project_number, user_name, test=False):
+    if test ==False:
+        project_path = '/lustre/naasc/observers/' + user_name + '/'
+    else:
+        project_path='/lustre/naasc/users/elastufk/test/'
     os.chdir('/lustre/naasc/sciops/cycle2_release/') #assume cycle 2 ....
     tarlist = glob.glob('*%s*.tar' % project_number)
     if tarlist == []:
@@ -43,34 +47,34 @@ def stage_data(mous):
         casa_version = project_info.casa_version()
         asdm_uids = gasdm.main(original_mous)
         project_dict = {'project_type': 'Imaging', 'asdm': asdm_uids, 'casa_version': casa_version}
+        mpre.downloadASDM(project_dict)  # run asdm export light on the asdms
+        mpre.runPipeline(project_dict)
         return project_dict
-        #mpre.downloadASDM(project_dict)  # run asdm export light on the asdms
-        #mpre.runPipeline(project_dict)
 
 def start_process():
     multiprocessing.current_process().name 
 
-def staging(project_number, user_name):
-    sinfo = staging_info(project_number, user_name)
+def staging(project_number, user_name, test=False):
+    start_time=time.time()
+    sinfo = staging_info(project_number, user_name, test=test)
     os.chdir('%s' % sinfo[0])
     tarlist = sinfo[2]
     
     #untar stuff
-    
+    os.chdir('data')  
     for tarball in tarlist:
-        if os.path.isfile('/lustre/naasc/sciops/cycle2_release/%s' % tarball):
-            #os.system('cp /lustre/naasc/sciops/cycle2_release/%s %s/data/' % (tarball, sinfo[0])) 
-            print tarball
-        elif os.path.isfile('/lustre/naasc/sciops/cycle1_release/%s' % tarball):
-            #os.system('cp /lustre/naasc/sciops/cycle1_release/%s %s/data/' % (tarball, sinfo[0])) 
-            print tarball
-        elif os.path.isfile('/lustre/naasc/sciops/cycle3_release/%s' % tarball):
-            #os.system('cp /lustre/naasc/sciops/cycle3_release/%s %s/data/' % (tarball, sinfo[0]))
-            print tarball 
+        if os.path.isfile('/lustre/naasc/sciops/cycle2_release/%s' % tarball) == True:
+            os.system('cp /lustre/naasc/sciops/cycle2_release/%s %sdata/' % (tarball, sinfo[0])) 
+            #print tarball
+        elif os.path.isfile('/lustre/naasc/sciops/cycle1_release/%s' % tarball) == True:
+            os.system('cp /lustre/naasc/sciops/cycle1_release/%s %sdata/' % (tarball, sinfo[0])) 
+            #print tarball
+        elif os.path.isfile('/lustre/naasc/sciops/cycle3_release/%s' % tarball) == True:
+            os.system('cp /lustre/naasc/sciops/cycle3_release/%s %sdata/' % (tarball, sinfo[0]))
+            #print tarball 
         else:
             sys.exit('ERROR: %s not found in cycleN_release folders!')
-        #os.system('tar -xvf %s' % tarball) #EL fix later
- 
+        os.system('tar -xf %s' % tarball) #who needs verbose?
     
     # change into directories, source pipeline, run scriptForPI
     project_path = sinfo[0] + 'data/' + sinfo[1] + '/sg_ouss_id/group_ouss_id/'
@@ -81,4 +85,5 @@ def staging(project_number, user_name):
     pool = multiprocessing.Pool(processes=pool_size, initializer=start_process,)
     pool_outputs= pool.map(stage_data, mous_glob)
     print pool_outputs
-    
+    print("--- %s seconds ---" % (time.time() - start_time))
+
